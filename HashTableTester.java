ea73ad68308e432e9aff8d6b971568a0f6fc0aa1
Shreyam924 <147807444+Shreyam924@users.noreply.github.com>
2024-06-06 15:41:22 +0530

    Update HashTableTester.java

--- HashTableTester.java
+++ HashTableTester.java
@@ -1,10 +1,13 @@
 
 public class HashTableTester {
-	public static void main(String[] args) {
-		HashTable ht=new HashTable();
-		ht.put("java","programming");
-		System.out.println(ht.get("java"));
-		ht.remove("java");
-		System.out.println(ht.get("java"));	
-	}
+    public static void main(String[] args) {
+        HashTable ht = new HashTable();
+        System.out.println("Initial size:" + ht.size());
+        ht.put("java", "programming");
+        System.out.println("Size after insertion:" + ht.size());
+        System.out.println("Value for java key:" + ht.get("java"));
+        ht.remove("java");
+        System.out.println("Size after removal:" + ht.size());
+        System.out.println("Value for java key after removal:" + ht.get("java"));
+    }
 }
